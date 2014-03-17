a582309e8fd609639b5649995052fc3897914a20
netankit <ankit.bahuguna@cs.tum.edu>
2014-03-17 17:35:20 +0100

    Customized the ESGraph and ESGraph Average to run from commandline

--- src/ESGraph.java
+++ src/ESGraph.java
@@ -9,51 +9,62 @@ public class ESGraph {
 
 	public static void main(String[] args) {
 
-		String inputFile = "bulkTookFile.txt";
-		try {
-			System.out.println("Execution Started.........");
-			// Open the file that is the first
-			// command line parameter
-			FileInputStream fstream = new FileInputStream(inputFile);
-			// Get the object of DataInputStream
-			DataInputStream in = new DataInputStream(fstream);
-			// Writes to the file named "inputFileName_trie_out"
-			FileWriter foutstream = new FileWriter("output.csv");
-			BufferedWriter out = new BufferedWriter(foutstream);
+		if (args.length != 3) {
+			System.out
+					.println("USAGE:ESEvaluation <inputFile> <outputfile_csv> <first_segment_size>");
+		} else {
+			try {
+				String inputFile = args[0];
+				String outputfile_csv = args[1];
+				int first_segment_size = Integer.parseInt(args[2]);
 
-			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+				System.out.println("Execution Started.........");
+				// Open the file that is the first
+				// command line parameter
+				FileInputStream fstream = new FileInputStream(inputFile);
+				// Get the object of DataInputStream
+				DataInputStream in = new DataInputStream(fstream);
+				// Writes to the file named "inputFileName_trie_out"
+				FileWriter foutstream = new FileWriter(outputfile_csv);
 
-			String strLine;
-			long count = 1;
-			long subcount = 1;
-			long chunkcount = 1;
-			String outputText;
-			String[] lineFragment;
-			// Read File Line By Line
-			while ((strLine = br.readLine()) != null) {
-				if (count % 20 == 0) {
-					if (subcount % 2000 == 0) {
-						lineFragment = strLine.split(" ");
-						outputText = chunkcount + "," + lineFragment[3] + "\n";
-						out.write(outputText);
-						chunkcount++;
-						subcount++;
-						count++;
+				BufferedWriter out = new BufferedWriter(foutstream);
+
+				BufferedReader br = new BufferedReader(
+						new InputStreamReader(in));
+
+				String strLine;
+				long count = 1;
+				long subcount = 1;
+				long chunkcount = 1;
+				String outputText;
+				String[] lineFragment;
+				// Read File Line By Line
+				while ((strLine = br.readLine()) != null) {
+					if (count % first_segment_size == 0) {
+						if (subcount % first_segment_size == 0) {
+							lineFragment = strLine.split(" ");
+							outputText = chunkcount + "," + lineFragment[3]
+									+ "\n";
+							out.write(outputText);
+							chunkcount++;
+							subcount++;
+							count++;
+						} else {
+							subcount++;
+						}
+
 					} else {
-						subcount++;
+						count++;
 					}
 
-				} else {
-					count++;
 				}
-
+				// Close the input stream
+				in.close();
+				out.close();
+				System.out.println("### Execution Completed ###");
+			} catch (Exception e) {// Catch exception if any
+				System.err.println("Error: " + e.getMessage());
 			}
-			// Close the input stream
-			in.close();
-			out.close();
-			System.out.println("### Execution Completed ###");
-		} catch (Exception e) {// Catch exception if any
-			System.err.println("Error: " + e.getMessage());
 		}
 	}
 }
--- src/ESGraphAverage.java
+++ src/ESGraphAverage.java
@@ -7,95 +7,119 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class generates a CSV file consisting of the average values of execution
+ * time.
+ * 
+ * @author ankit
+ * 
+ */
 public class ESGraphAverage {
 
 	public static void main(String[] args) {
+		if (args.length != 4) {
+			System.out
+					.println("USAGE:ESGraph <inputFile> <outputfile_csv> <first_segment_size> <second_segment_size>");
+		} else {
 
-		String inputFile = "bulkTookFile.txt";
-		try {
-			System.out.println("Execution Started.........");
-			// Open the file that is the first
-			// command line parameter
-			FileInputStream fstream = new FileInputStream(inputFile);
-			// Get the object of DataInputStream
-			DataInputStream in = new DataInputStream(fstream);
-			// Writes to the file named "inputFileName_trie_out"
-			FileWriter foutstream = new FileWriter("output_avg.csv");
-			BufferedWriter out = new BufferedWriter(foutstream);
+			String inputFile = args[0];
+			String outputfile_csv = args[1];
+			int first_segment_size = Integer.parseInt(args[2]);
+			int second_segment_size = Integer.parseInt(args[3]);
 
-			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			try {
+				System.out.println("Execution Started.........");
+				// Open the file that is the first
+				// command line parameter
+				FileInputStream fstream = new FileInputStream(inputFile);
+				// Get the object of DataInputStream
+				DataInputStream in = new DataInputStream(fstream);
+				// Writes to the file
+				FileWriter foutstream = new FileWriter(outputfile_csv);
+				BufferedWriter out = new BufferedWriter(foutstream);
 
-			String strLine;
+				BufferedReader br = new BufferedReader(
+						new InputStreamReader(in));
 
-			String[] lineFragment;
+				String strLine;
 
-			List<Long> myTwenty = new ArrayList<Long>();
-			List<Long> myTwoThousand = new ArrayList<Long>();
-			long lineNumber = 1;
-			long averageval;
-			long totalSum = 0;
+				String[] lineFragment;
 
-			// Read File Line By Line
-			while ((strLine = br.readLine()) != null) {
-				if (strLine.startsWith("bulk took : ")) {
-					// System.out.println("i am in");
-					lineFragment = strLine.split(" ");
-					totalSum = totalSum + Integer.parseInt(lineFragment[3]);
-					if (lineNumber % 20 == 0) {
-						// System.out.println("i am here too");
-						averageval = calculateAverage(totalSum, 20);
-						myTwenty.add(averageval);
-						totalSum = 0;
-						averageval = 0;
-					}
-					lineNumber++;
-				} else
-					continue;
-			}
-			System.out.println("LN: " + lineNumber);
+				List<Long> myTwenty = new ArrayList<Long>();
+				List<Long> myTwoThousand = new ArrayList<Long>();
+				long lineNumber = 1;
+				long averageval;
+				long totalSum = 0;
 
-			System.out.println(myTwenty.size());
-
-			System.out.println("myTwoTwentyArray Populated");
-
-			long arrayCount = 1;
-			long totalSumChunk = 0;
-			long averagevalchunk;
-
-			// Finding average for every 2000 values in the arraylist
-			for (long i = 0; i < myTwenty.size(); i++) {
-				totalSumChunk += myTwenty.get((int) i);
-				if (arrayCount % 50 == 0) {
-					// System.out.println("i am here too");
-					averagevalchunk = calculateAverage(totalSumChunk, 50);
-					myTwoThousand.add(averagevalchunk);
-					totalSumChunk = 0;
-					averagevalchunk = 0;
+				// Read File Line By Line
+				while ((strLine = br.readLine()) != null) {
+					if (strLine.startsWith("bulk took : ")) {
+						// System.out.println("i am in");
+						lineFragment = strLine.split(" ");
+						totalSum = totalSum + Integer.parseInt(lineFragment[3]);
+						// totalSum = totalSum +
+						// Integer.parseInt(lineFragment[6]);
+						if (lineNumber % first_segment_size == 0) {
+							// System.out.println("i am here too");
+							averageval = calculateAverage(totalSum,
+									first_segment_size);
+							myTwenty.add(averageval);
+							totalSum = 0;
+							averageval = 0;
+						}
+						lineNumber++;
+					} else
+						continue;
 				}
-				arrayCount++;
+				System.out.println("Total Input Values: " + lineNumber);
+				System.out
+						.println("Total Input Values after First Segmentation: "
+								+ myTwenty.size());
+				System.out.println("First Segment Populated");
 
+				long arrayCount = 1;
+				long totalSumChunk = 0;
+				long averagevalchunk;
+
+				// Finding average for every 50 values in the arraylist
+				for (long i = 0; i < myTwenty.size(); i++) {
+					totalSumChunk += myTwenty.get((int) i);
+					if (arrayCount % second_segment_size == 0) {
+						// System.out.println("i am here too");
+						averagevalchunk = calculateAverage(totalSumChunk,
+								second_segment_size);
+						myTwoThousand.add(averagevalchunk);
+						totalSumChunk = 0;
+						averagevalchunk = 0;
+					}
+					arrayCount++;
+
+				}
+				// System.out.println(myTwoThousand.get(1));
+				// System.out.println(myTwoThousand.get(5));
+				System.out
+						.println("Number of Values in Second Segment/ Total Output Values : "
+								+ myTwoThousand.size());
+				System.out.println("Second Segment Populated");
+
+				String outputTextAvg;
+				for (long chunkCountTemp = 0; chunkCountTemp < myTwoThousand
+						.size(); chunkCountTemp++) {
+					outputTextAvg = chunkCountTemp + 1 + ","
+							+ myTwoThousand.get((int) chunkCountTemp) + "\n";
+					out.write(outputTextAvg);
+				}
+				System.out.println("Writing to File Completed");
+
+				// Close the input stream
+				in.close();
+				out.close();
+				System.out.println("### Execution Completed ###");
+			} catch (Exception e) {// Catch exception if any
+				System.err.println("Error: " + e.getMessage());
 			}
-			System.out.println(myTwoThousand.get(1));
-			System.out.println(myTwoThousand.get(5));
 
-			System.out.println("myTwoThousandArray Populated");
-
-			String outputTextAvg;
-			for (long chunkCountTemp = 0; chunkCountTemp < myTwoThousand.size(); chunkCountTemp++) {
-				outputTextAvg = chunkCountTemp + 1 + ","
-						+ myTwoThousand.get((int) chunkCountTemp) + "\n";
-				out.write(outputTextAvg);
-			}
-			System.out.println("Writing to File Completed");
-
-			// Close the input stream
-			in.close();
-			out.close();
-			System.out.println("### Execution Completed ###");
-		} catch (Exception e) {// Catch exception if any
-			System.err.println("Error: " + e.getMessage());
 		}
-
 	}
 
 	private static long calculateAverage(long totalSum, int size) {
